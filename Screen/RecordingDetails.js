1108c1644ca79a0b6cc49a3890fe7c546b6ed4ee
VivekBansalBoxinall <vivekbansal@boxinallsoftech.com>
2024-05-18 11:29:38 +0530

    Icons done

--- Screen/RecordingDetails.js
+++ Screen/RecordingDetails.js
@@ -1,19 +1,30 @@
 import {StyleSheet, Text, View} from 'react-native';
+import {colorData} from '../utils/colorData';
 import React from 'react';
 
 const RecordingDetails = () => {
   return (
-    <View>
-      <Text
-        style={{
-          textAlign: 'center',
-          fontSize: 20,
-          fontWeight: 'bold',
-          marginTop: 50,
-          color: 'black',
-        }}>
-        RecordingDetails
-      </Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: colorData.back2,
+        marginTop: 52,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        marginHorizontal: 0.5,
+      }}>
+      <View>
+        <Text
+          style={{
+            textAlign: 'center',
+            fontSize: 20,
+            fontWeight: 'bold',
+            marginTop: 50,
+            color: 'white',
+          }}>
+          RecordingDetails
+        </Text>
+      </View>
     </View>
   );
 };
